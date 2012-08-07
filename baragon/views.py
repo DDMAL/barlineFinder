02ea8125ea0e752d4a11b0ea1c65eba924912914
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-08-07 13:25:34 -0400

    Upload Image Conversion
    From any image format readable by PIL to .tiff, which gamera can import.
    Fixes #11

--- baragon/views.py
+++ baragon/views.py
@@ -1,9 +1,14 @@
 from django.template import RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.core.files.base import ContentFile
 
 from baragon.models import Page
 from baragon.forms import UploadImageForm
+
+import os
+from PIL import Image
+from StringIO import StringIO
 
 def home(request):
     '''
@@ -21,8 +26,21 @@ def upload(request):
         # bind the data to the form
         form = UploadImageForm(request.POST, request.FILES)
         if form.is_valid():
-            page = Page(image_file=request.FILES['page_image'])
-            page.save()
+            # check uploaded image is a tiff file
+            input_file = request.FILES['page_image']
+            filename, input_ext = os.path.splitext(input_file.name)
+            if input_ext != '.tiff':
+                # convert the image to a tiff file
+                uploaded_img = Image.open(StringIO(input_file.read()))
+                new_filename = '%s.tiff' % filename
+                converted_img = StringIO()
+                uploaded_img.save(converted_img, 'TIFF')
+                page = Page()
+                page.image_file.save(new_filename, ContentFile(converted_img.getvalue()), save=True)
+            else:
+                page = Page(image_file=request.FILES['page_image'])
+                page.save()
+
             # for now, display the database tuple until we figure out what to do with the image
             return HttpResponse('page id: %d, path: %s, height: %d, width: %d' % (page.id, page.image_file, page.height, page.width))
     else:
