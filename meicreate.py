a6a0f01ffbff9192612190c1a53aacd2de538a47
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-08-15 16:46:37 -0400

    Write mei system breaks
    The barline data conversion program now writes system breaks and writes
    the number of staves according to the staff group input provided by the
    user. Still does not handle user input such as 'x3' for repeating 3
    times, or '|' to specify bar through. Also does not handle the case
    where the staff groupings change on a page, e.g., (2)x2 (4(2))x2

--- meicreate.py
+++ meicreate.py
@@ -24,6 +24,8 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+Sample usage:
+python meicreate.py -b ../Results/N_10_D_kl_Baermann_GA_cl_002_bar_position_1.txt -s ../Results/N_10_D_kl_Baermann_GA_cl_002_staff_vertices.txt -f detmoldbars.mei -g '(1)' -v    
 """
 
 import argparse
@@ -34,122 +36,203 @@ from pymei import MeiDocument, MeiElement, XmlExport
 
 # set up command line argument structure
 parser = argparse.ArgumentParser(description='Convert text file of OMR barline data to mei.')
-parser.add_argument('-fin', '--filein', help='input file')
-parser.add_argument('-sgs', '--staffgroups', help='staffgroups')
-parser.add_argument('-fout', '--fileout', help='output file')
+parser.add_argument('-b', '--barfilein', help='barline data input file')
+parser.add_argument('-s', '--stafffilein', help='staff data input file')
+parser.add_argument('-g', '--staffgroups', help='staffgroups')
+parser.add_argument('-f', '--fileout', help='output file')
 parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
 
-def bardata_to_mei(input_path, staff_groups, output_path, verbose):
-    meidoc = MeiDocument()
-    mei = MeiElement('mei')
-    meidoc.setRootElement(mei)
+class BarlineDataConverter:
+    '''
+    Convert the output of the barline detection algorithm
+    to MEI.
+    '''
 
-    ###########################
-    #         MetaData        #
-    ###########################
-    mei_head = MeiElement('meihead')
-    mei.addChild(mei_head)
+    def __init__(self, bar_input_path, staff_input_path, verbose):
+        '''
+        Initialize the converter
+        '''
 
-    ###########################
-    #           Body          #
-    ###########################
-    music = MeiElement('music')
-    body = MeiElement('body')
-    mdiv = MeiElement('mdiv')
-    score = MeiElement('score')
-    score_def = MeiElement('score_def')
-    section = MeiElement('section')
+        self.bar_input_path = bar_input_path
+        self.staff_input_path = staff_input_path
+        self.verbose = verbose
 
-    # physical location data
-    facsimile = MeiElement('facsimile')
-    surface = MeiElement('surface')
+    def bardata_to_mei(self, sg_hint):
+        '''
+        Perform the data conversion to mei
+        '''
 
-    # generate staff group
-    parser = nestedExpr()
-    sg_list = parser.parseString(staff_groups).asList()[0]
-    staff_grp, n = _create_staff_group(sg_list, MeiElement('staffGrp'), 0)
-    if verbose:
-        print "number of staves: %d" % n
+        self.meidoc = MeiDocument()
+        mei = MeiElement('mei')
+        self.meidoc.setRootElement(mei)
 
-    mei.addChild(music)
-    music.addChild(facsimile)
-    facsimile.addChild(surface)
-    music.addChild(body)
-    body.addChild(mdiv)
-    mdiv.addChild(score)
-    score.addChild(score_def)
-    score_def.addChild(staff_grp)
-    score.addChild(section)
+        ###########################
+        #         MetaData        #
+        ###########################
+        mei_head = MeiElement('meiHead')
+        mei.addChild(mei_head)
 
-    with open(input_path, 'r') as bar_output:
-        cur_staff = None
-        cur_layer = None
-        for i, barline in enumerate(bar_output):
-            barline = barline.split("\t")
+        ###########################
+        #           Body          #
+        ###########################
+        music = MeiElement('music')
+        body = MeiElement('body')
+        mdiv = MeiElement('mdiv')
+        score = MeiElement('score')
+        score_def = MeiElement('scoreDef')
+        section = MeiElement('section')
 
-            staff_num = barline[0]
-            if cur_staff is None or staff_num != cur_staff.getAttribute('n').value:
-                # a new staff
-                cur_staff = MeiElement('staff')
-                cur_staff.addAttribute('n', staff_num)
-                cur_layer = MeiElement('layer')
-                cur_layer.addAttribute('n', '1')
-                section.addChild(cur_staff)
-                cur_staff.addChild(cur_layer)
+        # physical location data
+        facsimile = MeiElement('facsimile')
+        surface = MeiElement('surface')
 
-            ulx = barline[1]
-            uly = barline[2]
-            lrx = barline[3]
-            lry = barline[4]
+        # generate staff group
+        parser = nestedExpr()
+        sg_list = parser.parseString(sg_hint).asList()[0]
+        staff_grp, n = self._create_staff_group(sg_list, MeiElement('staffGrp'), 0)
+        if self.verbose:
+            print "number of staves: %d" % n
 
-            barline = MeiElement('barLine')
-            zone = _create_zone(ulx, uly, lrx, lry)
-            facsimile.addChild(zone)
-            barline.addAttribute('facs', zone.getId())
-            cur_layer.addChild(barline)
+        mei.addChild(music)
+        music.addChild(facsimile)
+        facsimile.addChild(surface)
+        
+        # get bounding box information of staves
+        layout = MeiElement('layout')
+        page = MeiElement('page')
+        music.addChild(layout)
+        layout.addChild(page)
 
-        if verbose:
-            print "%d barlines processed." % i
+        # this encoding will work for one-staff systems like in liber
+        # TODO: find out how to attach facs data to an individual staff in a system
+        with open(self.staff_input_path, 'r') as staff_output:
+            for staff_bb in staff_output:
+                # get bounding box of the staff
+                staff_bb = staff_bb.split("\t")[1:]
+                zone = self._create_zone(staff_bb[0], staff_bb[1], staff_bb[2], staff_bb[3])
+                surface.addChild(zone)
+                system = MeiElement('system')
+                system.addAttribute('facs', zone.getId())
+                page.addChild(system)
+        
+        music.addChild(body)
+        body.addChild(mdiv)
+        mdiv.addChild(score)
+        score.addChild(score_def)
+        score_def.addChild(staff_grp)
+        score.addChild(section)
 
-    # output mei file
-    XmlExport.meiDocumentToFile(meidoc, output_path)
+        staff_defs = staff_grp.getDescendantsByName('staffDef')
+        system_refs = page.getChildrenByName('system')
+        with open(self.bar_input_path, 'r') as bar_output:
+            cur_staff = None
+            cur_layer = None
+            cur_staff_num = 0
+            for i, barline in enumerate(bar_output):
+                barline = barline.split("\t")
 
-def _create_staff_group(sg_list, staff_grp, n):
-    if not sg_list:
-        return staff_grp, n
-    else:
-        if type(sg_list[0]) is list:
-            new_staff_grp, n = _create_staff_group(sg_list[0], MeiElement('staffGrp'), n)
-            staff_grp.addChild(new_staff_grp)
+                staff_num = int(barline[0])
+                if staff_num != cur_staff_num:
+                    # a new staff or system
+                    if cur_staff is None or staff_num % len(staff_defs) != 0:
+                        # a new staff
+                        cur_staff = MeiElement('staff')
+                        cur_staff.addAttribute('n', str(staff_num % len(staff_defs) + 1))
+                        cur_layer = MeiElement('layer')
+                        cur_layer.addAttribute('n', '1')
+                        if staff_num == 1:
+                            sb = self._create_sb(system_refs, staff_num)
+                            cur_layer.addChild(sb)
+
+                        section.addChild(cur_staff)
+                        cur_staff.addChild(cur_layer)
+                    else:
+                        # a new system break
+                        sb = self._create_sb(system_refs, staff_num)
+                        cur_layer.addChild(sb)
+
+                cur_staff_num = staff_num
+                        
+                ulx = barline[1]
+                uly = barline[2]
+                lrx = barline[3]
+                lry = barline[4]
+
+                barline = MeiElement('barLine')
+                zone = self._create_zone(ulx, uly, lrx, lry)
+                surface.addChild(zone)
+                barline.addAttribute('facs', zone.getId())
+                cur_layer.addChild(barline)
+
+            if self.verbose:
+                print "%d barlines processed." % i
+
+    def output_mei(self, output_path):
+        '''
+        Write the generated mei to disk
+        '''
+
+        # output mei file
+        XmlExport.meiDocumentToFile(self.meidoc, output_path)
+
+    def _create_staff_group(self, sg_list, staff_grp, n):
+        '''
+        Recursively create the staff group element from the parsed
+        user input of the staff groupings
+        '''
+        if not sg_list:
+            return staff_grp, n
         else:
-            n_staff_defs = int(sg_list[0])
-            # get current staffDef number
-            for i in range(n_staff_defs):
-                staff_def = MeiElement('staffDef')
-                staff_def.addAttribute('n', str(n+i+1))
-                staff_grp.addChild(staff_def)
-            n += n_staff_defs
+            if type(sg_list[0]) is list:
+                new_staff_grp, n = self._create_staff_group(sg_list[0], MeiElement('staffGrp'), n)
+                staff_grp.addChild(new_staff_grp)
+            else:
+                n_staff_defs = int(sg_list[0])
+                # get current staffDef number
+                for i in range(n_staff_defs):
+                    staff_def = MeiElement('staffDef')
+                    staff_def.addAttribute('n', str(n+i+1))
+                    staff_grp.addChild(staff_def)
+                n += n_staff_defs
 
-        return _create_staff_group(sg_list[1:], staff_grp, n)
+            return self._create_staff_group(sg_list[1:], staff_grp, n)
 
-def _create_zone(ulx, uly, lrx, lry):
-    zone = MeiElement('zone')
-    zone.addAttribute('ulx', ulx)
-    zone.addAttribute('uly', uly)
-    zone.addAttribute('lrx', lrx)
-    zone.addAttribute('lry', lry)
+    def _create_sb(self, system_refs, staff_num):
+        '''
+        Create a system break element and attach it to a system reference
+        '''
 
-    return zone
+        sb = MeiElement('sb')
+        sb.addAttribute('systemref', str(system_refs[staff_num-1].getId()))
+        sb.addAttribute('n', str(staff_num))
+
+        return sb
+
+    def _create_zone(self, ulx, uly, lrx, lry):
+        '''
+        Create a zone element
+        '''
+
+        zone = MeiElement('zone')
+        zone.addAttribute('ulx', ulx)
+        zone.addAttribute('uly', uly)
+        zone.addAttribute('lrx', lrx)
+        zone.addAttribute('lry', lry)
+
+        return zone
 
 if __name__ == "__main__":
     # parse command line arguments
     args = parser.parse_args()
 
-    input_path = args.filein
-    if not os.path.exists(input_path):
+    bar_input_path = args.barfilein
+    staff_input_path = args.stafffilein
+    if not os.path.exists(bar_input_path) or not os.path.exists(staff_input_path):
         raise ValueError('The input file does not exist')
 
     output_path = args.fileout
-    staff_groups = args.staffgroups
+    sg_hint = args.staffgroups
     verbose = args.verbose
-    bardata_to_mei(input_path, staff_groups, output_path, verbose)
+    bar_converter = BarlineDataConverter(bar_input_path, staff_input_path, verbose)
+    bar_converter.bardata_to_mei(sg_hint)
+    bar_converter.output_mei(output_path)
